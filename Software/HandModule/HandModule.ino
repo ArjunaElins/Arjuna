3e7a87323a151e85ad88e8b1b15119c725d20134
Ilham Imaduddin <ilham.imaduddin@mail.ugm.ac.id>
2015-08-23 13:01:13 +0700

    Add RF24 Support

--- Software/HandModule/HandModule.ino
+++ Software/HandModule/HandModule.ino
@@ -27,6 +27,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+ 
+#include <SPI.h>
+#include <printf.h>
+#include <RF24.h>
 
 class Motor
 {
@@ -94,14 +98,35 @@ class Motor
     }
 };
 
-#define MOTOR_COUNT   10
+#define MOTOR_COUNT     10
+#define MOTOR_OFF       0x80
+#define MOTOR_ON        0x90
+#define ALL_MOTOR_OFF   0xC0
+#define ALL_MOTOR_ON    0xD0
 
+RF24 radio(18, 10);
+byte data;
 Motor motor[MOTOR_COUNT];
 int motorPin[MOTOR_COUNT] = {1, 0, 2, 3, 4, 5, 6, 7, 8, 9};
 
 void setup()
 {
+  /* Initialize RF Address */
+  byte address[6] = "ArS01";
+
+  /* Initialize serial communication */
   Serial.begin(9600);
+  printf_begin();
+
+  /* Initialize radio */
+  Serial.println("Initializing radio...");
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(1);
+  radio.setAutoAck(1);
+  radio.openReadingPipe(1, address);
+  radio.startListening();
+  Serial.println("Radio is listening...");
 
   // Initializing Motor
   for (int i = 0; i < MOTOR_COUNT; i++)
@@ -113,13 +138,53 @@ void setup()
 
 void loop()
 {
+  if (radio.available())
+  {
+    while(radio.available());
+    {
+      radio.read(&data, 1);
 
+      Serial.print("Data received: ");
+      Serial.println(data, HEX);
+      parseCommand(data);
+    }
+  }
   
   // Run Motor Timeout Controller
   for (int i = 0; i < MOTOR_COUNT; i++)
   {
     motor[i].timeoutControl();
   }
+}
+
+/**
+ * Parse command
+ * 
+ * @param   message   command message
+ */
+void parseCommand(byte message)
+{
+  byte command = message & 0xF0;
+  byte channel = message & 0x0F;
+
+  if (channel > 9)
+    return;
+
+   switch(command)
+   {
+      case MOTOR_OFF:
+        motor[channel].turnOff();
+        break;
+      case MOTOR_ON:
+        motor[channel].turnOn();
+        break;
+      case ALL_MOTOR_OFF:
+        allMotorOff();
+        break;
+      case ALL_MOTOR_ON:
+        allMotorOn(); 
+        break;
+   }
 }
 
 /**
